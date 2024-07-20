dfe500aa839cb31fe70b9ff84e82f6e466215b51
UsmanBasharat <ChUsman215@gmail.com>
2024-07-20 22:40:29 +0500

    First file.

---
+++ Cleancity.py
@@ -0,0 +1,9 @@
+city=["Lahore","Islamabad","Dadyal","Kotli","Faislaabad"]
+user_input=input("Enter your city:")
+R = "You need some improvement to make your city clean"
+for i in city:
+    if (i == user_input):
+        R = "Beautiful city"
+print(R)
+
+    
---
+++ studentgrades.py
@@ -0,0 +1,8 @@
+marks=float(input("Enter your marks:" ))
+if marks>=90:
+    print("A+") 
+elif marks>=80:
+    print("B+")
+else:
+ print("You are fail")
+    
